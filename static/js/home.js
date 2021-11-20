d1de348e21ed6970dd145cdcc10a19222da0ed2a
Mahadi karim Munif <mahadikarimmunif@gmail.com>
2021-11-20 23:14:35 +0600

    by mahadi

---
+++ static/js/home.js
@@ -0,0 +1,9 @@
+function slide(){
+    document.getElementById("sidebar").style.width="300px";
+    document.getElementById("menu").style.visibility="visible"
+    
+}
+function inside(){
+    document.getElementById("sidebar").style.width="0px";
+    document.getElementById("menu").style.visibility="hidden"
+}
