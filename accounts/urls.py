23e6f7deb3c07abaec19e30b3024e015148bdbb4
Mahadi karim Munif <mahadikarimmunif@gmail.com>
2021-12-02 22:15:33 +0600

    implemented password reset mail

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .import views
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('register/', views.studentRegisterPage, name='register'),
     path('InstructorRegister/', views.instructorRegisterPage, name='instructorRegisterPage'),
@@ -19,5 +21,10 @@ urlpatterns = [
     path('EditInstructorProfile/',views.editInstructorProfile,name='editInstructorProfile'),
     
     path('Section/',views.section, name='section'),
+    
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='password_reset.html'),name='reset_password'),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
     
 ]
