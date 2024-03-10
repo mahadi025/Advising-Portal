699b412a5916503d44a4debfcce935514929ba8b
mahadi025 <mahadikarimmunif@gmail.com>
2024-03-10 20:53:07 +0600

    added year and semester to home page

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+year = settings.YEAR
+semester = settings.SEMESTER
 
 
 @login_required(login_url="login")
 def home(request):
-    return render(request, "home/home.html")
+    context = {"year": year, "semester": semester}
+    return render(request, "home/home.html", context)
